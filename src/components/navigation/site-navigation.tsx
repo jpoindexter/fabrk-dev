1ea8a6b99c47a6921312cb3091a8e87c7e6a7a51
Jason Poindexter <jpoindexter@users.noreply.github.com>
2025-12-12 11:52:02 +0100

    fix: Remove GET STARTED button from SiteNavigation (docs pages)
    Removed GET STARTED button from:
    - Desktop SiteNavigation bar
    - Mobile sheet menu
    - Mobile CTA button
    
    This is the actual navigation component used on /docs pages

--- src/components/navigation/site-navigation.tsx
+++ src/components/navigation/site-navigation.tsx
@@ -119,9 +119,6 @@ export function SiteNavigation() {
             <Button variant="outline" asChild className={cn(mode.radius, mode.font, 'text-xs')}>
               <Link href="/library">&gt; VIEW LIBRARY</Link>
             </Button>
-            <Button asChild className={cn(mode.radius, mode.font, 'text-xs')}>
-              <Link href="/#pricing">&gt; GET STARTED</Link>
-            </Button>
           </div>
         </motion.div>
 
@@ -198,20 +195,10 @@ export function SiteNavigation() {
                       <Link href="/library">&gt; VIEW LIBRARY</Link>
                     </Button>
                   </SheetClose>
-                  <SheetClose asChild>
-                    <Button className={cn(mode.radius, mode.font, 'mt-4 w-full text-xs')} asChild>
-                      <Link href="/#pricing">&gt; GET STARTED</Link>
-                    </Button>
-                  </SheetClose>
                 </div>
               </nav>
             </SheetContent>
           </Sheet>
-
-          {/* Mobile CTA Button */}
-          <Button asChild className={cn(mode.radius, mode.font, 'text-xs md:hidden')}>
-            <Link href="/#pricing">&gt; START</Link>
-          </Button>
         </motion.div>
       </div>
     </nav>
